c7f174e5da458d8c797b80d3c44f4ba77cb1d4f6
Ryan Dsouza <r.dsouza@newmotion.com>
2024-08-18 18:55:15 +0100

    setup CDK project with sensible defaults

--- bin/case-study.ts
+++ bin/case-study.ts
@@ -1,21 +1,18 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
+import { AwsSolutionsChecks } from 'cdk-nag';
+import { envs } from '../lib/utils';
 import { CaseStudyStack } from '../lib/case-study-stack';
 
 const app = new cdk.App();
 new CaseStudyStack(app, 'CaseStudyStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  env: {
+    account: envs.AWS_ACCOUNT_ID,
+    region: envs.AWS_REGION,
+  },
+  removeResourcesOnStackDeletion: true,
 });
+
+// Add the cdk-nag AwsSolutions Pack with extra verbose logging enabled.
+cdk.Aspects.of(app).add(new AwsSolutionsChecks({ verbose: true }));
